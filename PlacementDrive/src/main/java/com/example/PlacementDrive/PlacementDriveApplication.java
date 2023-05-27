e3b4bcc275d4852ca6688b663e9f07f73a984d11
Piyush <piyushpathak3004@gmail.com>
2023-05-27 15:27:08 +0530

    Starting the Placement Drive Porject

---
+++ PlacementDrive/src/main/java/com/example/PlacementDrive/PlacementDriveApplication.java
@@ -0,0 +1,13 @@
+package com.example.PlacementDrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacementDriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacementDriveApplication.class, args);
+	}
+
+}
---
+++ PlacementDrive/src/test/java/com/example/PlacementDrive/PlacementDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PlacementDrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacementDriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
